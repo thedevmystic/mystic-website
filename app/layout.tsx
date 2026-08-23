c1b8ff3151188f2105a8c3a6610d7396513375b0
Surya! <thedevmystic@gmail.com>
2026-08-23 18:33:21 +0530

    feat: add top loading bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@
 
 import type { Metadata } from 'next';
 
+import NextTopLoader from 'nextjs-toploader';
+
 import Cursor from '@/components/Cursor';
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
@@ -20,6 +22,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className="min-h-screen bg-background text-on-background transition-colors duration-300 antialiased">
         <ThemeProvider>
           <Cursor />
+          <NextTopLoader color="var(--color-primary)" height={2} showSpinner={false} />
           <Navbar />
           {children}
           <Footer />
